fd6c25ad9a5b7003d75e2970093f1a2c70ee1720
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2022-10-13 03:04:18 +0200

    Add JaCoCo engine

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import kotlinx.kover.api.DefaultJacocoEngine
+
 plugins {
     kotlin("jvm") version "1.7.20"
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
@@ -5,6 +7,10 @@ plugins {
 
 repositories {
     mavenCentral()
+}
+
+kover {
+    engine.set(DefaultJacocoEngine)
 }
 
 dependencies {
